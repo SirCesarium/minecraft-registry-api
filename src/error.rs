278f8f6e9f0accddb601b9346114ae1d89314f8d
SirCesarium <239650612+SirCesarium@users.noreply.github.com>
2026-06-04 19:28:28 -0400

    chore: add xml errors

--- src/error.rs
+++ src/error.rs
@@ -4,4 +4,12 @@ use thiserror::Error;
 pub enum ApiError {
     #[error("HTTP request failed: {0}")]
     Http(#[from] reqwest::Error),
+    #[error("XML parsing failed: {0}")]
+    Xml(String),
+}
+
+impl From<String> for ApiError {
+    fn from(s: String) -> Self {
+        Self::Xml(s)
+    }
 }
